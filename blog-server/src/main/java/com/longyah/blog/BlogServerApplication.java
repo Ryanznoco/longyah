92f5f80ca75546edbdfb77f99877725d78bfb47b
RenQiang <810625938@qq.com>
2019-06-04 22:59:33 +0800

    项目初始化

---
+++ blog-server/src/main/java/com/longyah/blog/BlogServerApplication.java
@@ -0,0 +1,16 @@
+package com.longyah.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author RenQiang
+ */
+@SpringBootApplication
+public class BlogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogServerApplication.class, args);
+    }
+
+}
